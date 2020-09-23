5df353649e75a97e68207f961fa562df07935595
eruandou <zambra_nacho@hotmail.com>
2020-09-23 18:01:58 -0300

    SampleScene update

--- Assets/PlayerShootingController.cs
+++ Assets/PlayerShootingController.cs
@@ -11,7 +11,7 @@ public class PlayerShootingController : MonoBehaviour
 
 
 
-
+    /*
     public void Shoot(bool isEnemy, int bulletNumber, Vector2 startPosition)
     {
         GameObject bulletToSpawn;
@@ -44,7 +44,7 @@ public class PlayerShootingController : MonoBehaviour
     }
 
 
-
+    */
 
 
 
