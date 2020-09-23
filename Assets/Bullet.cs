9343a1144224be0d75ae762c2edc6ed47685b885
eruandou <zambra_nacho@hotmail.com>
2020-09-23 15:33:48 -0300

    First bullet (just class)

---
+++ Assets/Bullet.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+
+    [SerializeField] private float speed;
+    [SerializeField] private int damage;
+    private Vector2 direction;
+
+
+
+
+
+
+
+
+    private void Update()
+    {
+        transform.position += new Vector3(direction.x, direction.y, 0) * speed * Time.deltaTime;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+
+
+
+
+
+
+}
