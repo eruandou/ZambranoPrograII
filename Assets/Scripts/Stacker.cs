4d8b762d4507bdbc33536faac2ffea90994cd9fb
eruandou <zambra_nacho@hotmail.com>
2020-09-23 17:59:00 -0300

    Stacker class

---
+++ Assets/Scripts/Stacker.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public interface Istacker
+{
+
+    void Stack(Bullet newBullet);
+
+    void Unstack();
+
+    Bullet First();
+
+    bool IsStackEmpty();
+
+    void InitializeStack();
+
+}
+
+public class Stacker : Istacker
+{
+
+    BulletNode top;
+
+    [SerializeField] private int StackLimit;
+    private int stackedItems;
+
+
+
+
+    public void Stack (Bullet newBullet)
+    {
+        //Check maxed stacked items
+
+        if (stackedItems < StackLimit)
+        {
+
+            BulletNode newNode = new BulletNode(newBullet);
+
+            newNode.nextNode = top;
+
+            top = newNode;
+        }
+    }
+
+    public void Unstack()
+    {
+        top = top.nextNode;
+    }
+
+    public void InitializeStack()
+    {
+        top = null;
+    }
+
+
+    public Bullet First()
+    {
+        return top.storedBullet;
+    }
+
+    public bool IsStackEmpty()
+    {
+        return (top == null);
+    }
+
+
+
+
+
+
+
+
+}
