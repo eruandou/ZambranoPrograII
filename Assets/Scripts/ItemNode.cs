3c04a32607597f5276d9eaec1808ca96c568ff2e
eruandou <zambra_nacho@hotmail.com>
2020-09-26 18:23:10 -0300

    Nodes for item dynamic array

---
+++ Assets/Scripts/ItemNode.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemNode 
+{
+
+    public Items storedItem;
+
+    public ItemNode nextNode;
+
+    public int Index { get;  set; }
+
+
+
+    public ItemNode (Items newItem)
+    {
+        storedItem = newItem;        
+    }
+
+
+
+
+
+
+}
