69297f6ef2144f21c650b17e150cfdc7899a7509
eruandou <zambra_nacho@hotmail.com>
2020-09-26 18:23:38 -0300

    Super beta GameManager

---
+++ Assets/Scripts/Gamemanager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemanager : MonoBehaviour
+{
+    public static Gamemanager instance;
+
+    public UI UI;
+
+
+
+
+    private void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this);
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
