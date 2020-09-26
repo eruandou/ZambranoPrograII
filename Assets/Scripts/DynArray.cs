81243679f36fcd43be58f2d5976e679ac39ed65b
eruandou <zambra_nacho@hotmail.com>
2020-09-26 18:23:22 -0300

    Dynamic array (list) class

---
+++ Assets/Scripts/DynArray.cs
@@ -0,0 +1,202 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDynArray
+{
+    void Add(Items item);
+
+    void Remove(int index);
+
+    Items Retrieve(int index);
+
+    ItemNode RetrieveNode(int index);
+    ItemNode PreviousNode(int index);
+
+    void Initialize();
+
+    bool IsArrayEmpty();
+
+
+}
+
+
+
+
+public class DynArray : IDynArray
+{
+
+    public ItemNode root;
+    public int HeldItems { get; private set; }
+    public int ItemLimit { get; private set; } = 10;
+
+    private ItemNode emptyNode;
+
+
+    public DynArray()
+    {
+        Initialize();
+        emptyNode = new ItemNode(null);
+    }
+
+    public void Add (Items newItem)
+    {
+        if (HeldItems > ItemLimit)
+        {
+            return;
+        }
+        
+        ItemNode newNode = new ItemNode(newItem);
+
+
+        if (root == null)
+        {
+            root = newNode;
+            root.Index = 0;
+          
+        }
+
+        else
+        {
+            ItemNode currentNode = root;           
+            while (currentNode.nextNode != null)
+            {
+                currentNode = currentNode.nextNode;        
+            }
+
+            currentNode.nextNode = newNode;
+            newNode.Index = currentNode.Index + 1;
+
+        }
+
+
+        HeldItems++;
+    }
+
+
+
+
+    public Items Retrieve(int index)
+    {
+        //if (index < HeldItems) return emptyNode.storedItem;
+        //else
+        
+            ItemNode currentNode = root;
+
+
+            while (currentNode.Index != index)
+            {
+                currentNode = currentNode.nextNode;
+            }
+
+            //if (currentNode == null) return emptyNode.storedItem;
+            return currentNode.storedItem;
+
+        
+
+    }
+
+    public ItemNode RetrieveNode(int index)
+    {
+        ItemNode currentNode = root;
+
+        while (currentNode.Index != index)
+        {
+            currentNode = currentNode.nextNode;
+        }
+
+        return currentNode;
+    }
+
+
+
+    public ItemNode PreviousNode(int index)
+    {
+        ItemNode currentNode = root;
+
+        if (index == 0)
+        {
+            while (currentNode.Index != HeldItems)
+            {
+                currentNode = currentNode.nextNode;
+            }
+            
+        }
+
+        else
+        {
+            while (currentNode.Index != index - 1)
+            {
+                currentNode = currentNode.nextNode;
+            }
+
+        }
+
+        return currentNode;
+
+    }
+
+
+
+
+    public void Remove(int index)
+    {
+
+        if (HeldItems <= 0)
+        {
+            return;
+        }
+
+        if (index != 0)
+        {
+            ItemNode currentNode = root;
+            while (currentNode.Index < index - 1)
+            {
+                currentNode = currentNode.nextNode;
+            }
+
+            currentNode.nextNode = currentNode.nextNode.nextNode;
+
+            while (currentNode != null)
+            {
+                currentNode = currentNode.nextNode;
+                currentNode.Index -= 1;
+            }
+
+        }
+
+        else
+        {
+            root = root.nextNode;
+            ItemNode currentNode = root;
+
+            while (currentNode != null)
+            {
+                currentNode.Index -= 1;
+                currentNode = currentNode.nextNode;
+            }
+        }
+
+
+        HeldItems--;
+    }
+
+    public bool IsArrayEmpty()
+    {
+        return (root == null);
+    }
+
+    public void Initialize()
+    {
+        root = null;
+    }
+    
+
+
+
+
+
+
+
+
+}
