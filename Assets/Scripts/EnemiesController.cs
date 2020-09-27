336ac9305c44d801a1de12c72b911e63d2244a7b
eruandou <zambra_nacho@hotmail.com>
2020-09-26 23:34:44 -0300

    Enemy and Enemy Controller added

---
+++ Assets/Scripts/EnemiesController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesController : MonoBehaviour
+{
+
+
+
+
+    public void FreezeEnemiesActivator()
+    {
+        StartCoroutine(FreezeEnemies());
+    }
+
+    private IEnumerator FreezeEnemies()
+    {
+
+        Debug.Log("Freezing enemies");
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+
+        foreach (Enemy enemy in enemies)
+        {
+            enemy.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+            enemy.enabled = false;
+        }
+
+        yield return new WaitForSeconds(4);
+
+        Debug.Log("Defrosting enemies");
+        foreach (Enemy enemy in enemies)
+        {
+            enemy.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+            enemy.enabled = true;
+        }
+        Debug.Log("Coroutine ended succesfully");
+    }
+
+
+
+
+
+
+}
