7eda8bb3411e8e5deeee773193c9459027c377ff
eruandou <zambra_nacho@hotmail.com>
2020-09-27 22:22:29 -0300

    Enemy bullet (spore) anim and script

---
+++ Assets/Scripts/Spores.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spores : MonoBehaviour
+{
+
+    [SerializeField] private float fallingSpeed;
+
+    [SerializeField] private int damage;
+
+    [SerializeField] private float timerToDie;
+
+    [SerializeField] private float timeToFall;
+
+    private Animator anim;
+
+    public Vector2 InitialDirection { get; set; }
+
+
+
+
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        transform.position += new Vector3(InitialDirection.x, InitialDirection.y, 0) * fallingSpeed * Time.deltaTime;
+
+        if (timeToFall <= 0)
+        {
+            transform.position += new Vector3(0, -fallingSpeed * Time.deltaTime, 0);
+
+            timerToDie -= Time.deltaTime;
+
+            if (timerToDie <= 0)
+            {
+                Die();
+            }
+        }
+
+        else
+        {
+            timeToFall -= Time.deltaTime;
+        }
+
+    }
+
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Player player = collision.GetComponent<Player>();
+
+        if (player != null)
+        {
+            player.lifeController.GetDamage(damage);
+        }
+
+
+        Die();
+
+    }
+
+    private void Die()
+    {
+        StartCoroutine(Death());
+    }
+
+    private IEnumerator Death()
+    {
+        anim.SetTrigger("Die");
+
+        yield return new WaitForSeconds(0.2f);
+
+        Destroy(this.gameObject);
+    }
+
+}
