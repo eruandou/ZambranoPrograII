04fe12ee44815281fe5f94949dc770f19b748bdc
eruandou <zambra_nacho@hotmail.com>
2020-09-28 16:45:23 -0300

    Update enemies controller with spawn functions

--- Assets/Scripts/EnemiesController.cs
+++ Assets/Scripts/EnemiesController.cs
@@ -7,12 +7,39 @@ public class EnemiesController : MonoBehaviour
 
 
     public Player playerRef;
+    public DynQueue enemiesToSpawnQueue;
+
+    [SerializeField] private Enemy enemy1;
+    [SerializeField] private Enemy enemy2;
+    [SerializeField] private Enemy enemy3;
+
+    private Dictionary <int,Enemy> enemiesDictionary = new Dictionary <int, Enemy>();
+
+
+    private int spawnedEnemies;
+    [SerializeField] private int spawnLimit;
+
+    [SerializeField] private float retrieveTimeStart;
+    private float retrieveTime;
+
+    public bool CanRetrieve => retrieveTime <= 0 && !enemiesToSpawnQueue.IsQueueEmpty();
 
 
 
     private void Start()
     {
         playerRef = FindObjectOfType<Player>();
+        enemiesToSpawnQueue = new DynQueue();
+
+        enemiesDictionary.Add(1, enemy1);
+        enemiesDictionary.Add(2, enemy2);
+        enemiesDictionary.Add(3, enemy3);
+
+        GenerateNewBatchOfEnemies();
+
+        enemiesToSpawnQueue.OnDequeue += OnDequeueHandler;
+
+
     }
 
 
@@ -44,14 +71,35 @@ public class EnemiesController : MonoBehaviour
             enemy.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
             enemy.ChangeState(Enemy.EnemyStates.Idle);
             enemy.GetComponent<Animator>().enabled = true;
-            enemy.GetComponent<SpriteRenderer>().color = Color.white;
+            enemy.GetComponent<SpriteRenderer>().color = enemy.neutralColor;
         }
         Debug.Log("Coroutine ended succesfully");
     }
 
 
+    public void GenerateNewBatchOfEnemies()
+    {
+        for (int i = 0; i < spawnLimit; i++)
+        {           
+            enemiesToSpawnQueue.Enqueue(enemiesDictionary[Random.Range(1, 4)]);
+            spawnedEnemies++;
+            Debug.Log(enemiesToSpawnQueue);
+        }
+    }
+
+    private void Update()
+    {
+        retrieveTime -= Time.deltaTime;
+    }
+
+    private void OnDequeueHandler()
+    {
+        retrieveTime = retrieveTimeStart;
+    }
 
 
-
-
+    public int SpawnedEnemies()
+    {
+        return spawnedEnemies;
+    }
 }
