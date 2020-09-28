e3acebb59205d49a3b4bfc1c18369c2d64fb3b66
eruandou <zambra_nacho@hotmail.com>
2020-09-28 16:46:25 -0300

    Spawn point animation control

---
+++ Assets/Scripts/SpawnPoints.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoints : MonoBehaviour
+{
+
+
+    private Animator anim;
+
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();        
+    }
+
+
+
+    public void ActivatePortal()
+    {
+        StartCoroutine(ActivateAnimation());
+    }
+
+
+    private IEnumerator ActivateAnimation()
+    {        
+        anim.enabled = true;
+        anim.SetBool("Appear", true);
+
+        anim.SetBool("Dissapear", false);
+
+        yield return new WaitForSeconds(0.7f);
+
+        anim.SetBool("Appear", false);
+        anim.SetBool("Dissapear", true);
+
+        yield return new WaitForSeconds(0.5f);
+
+        anim.enabled = false;
+
+    }
+
+}
