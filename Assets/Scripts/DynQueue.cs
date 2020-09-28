c7e72bf07772d2f52172186fbbb9e294b05d32d3
eruandou <zambra_nacho@hotmail.com>
2020-09-28 13:16:14 -0300

    Queue class and enemynode

---
+++ Assets/Scripts/DynQueue.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public interface IDynQueue
+{
+    void Enqueue(Enemy enemy);
+
+    void Dequeue();
+
+    Enemy First();
+
+    EnemyNode FirstNode();
+    void InitializeQueue();
+
+    bool IsQueueEmpty();
+}
+
+
+public class DynQueue 
+{
+    EnemyNode first;
+
+    EnemyNode last;
+
+
+    public DynQueue()
+    {
+        InitializeQueue();
+    }
+
+
+
+
+
+    public void Enqueue (Enemy newEnemy)
+    {
+
+        EnemyNode newNode = new EnemyNode(newEnemy);
+
+        //If last isn't null, add the new node as next node
+        if (last != null)
+        {
+           
+            last.NextNode = newNode;
+        }
+
+      
+        last = newNode;
+
+        
+        if (first == null)
+        {
+         
+            first = last;
+        }
+    }
+
+
+    public void Dequeue()
+    {
+       //Change first node for next node
+        first = first.NextNode;
+
+        //If there's no next node, the queue is empty
+        if (first == null)
+        {
+            last = null;
+        }
+    }
+
+
+    public bool IsQueueEmpty()
+    {
+        return (last == null);
+    }
+
+    public Enemy First()
+    {
+        return first.EnemyData;
+    }
+
+    public EnemyNode FirstNode()
+    {
+        return first;
+    }
+
+    public void InitializeQueue()
+    {
+        first = null;
+        last = null;
+    }
+
+
+}
---
+++ Assets/Scripts/EnemyNode.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyNode
+{
+
+    public EnemyNode NextNode { get; set; }
+
+    public Enemy EnemyData { get; set; }
+
+
+    public EnemyNode(Enemy newEnemy)
+    {
+        this.EnemyData = newEnemy;
+        this.NextNode = null;
+    }
+
+
+
+
+
+
+
+
+
+
+}
