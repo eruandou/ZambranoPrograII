f0dcdba921900e675fd00c8e21416feef6dd8e60
eruandou <zambra_nacho@hotmail.com>
2020-09-29 00:34:03 -0300

    Function to add nick to highscores

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.UI;
+
+public  class GameOver : MonoBehaviour
+{
+
+    private string nickEntered;
+
+    [SerializeField] private Text text;
+
+    public void Save()
+    {
+        DataSaver.SavePlayers(Gamemanager.instance.ActualPoints, nickEntered);
+        
+    }
+
+
+    public void Load()
+    {
+        DataSaver.LoadPlayers();
+    }
+
+    public void ChangeNameEntered()
+    {
+        nickEntered = text.text;
+        Debug.Log($"new name is {nickEntered}");
+    }
+
+}
