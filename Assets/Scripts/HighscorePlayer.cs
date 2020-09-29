edf56a7244842010f98dcfd7526519b61b19d8c6
eruandou <zambra_nacho@hotmail.com>
2020-09-28 21:04:30 -0300

    Player class for highscore keeping

---
+++ Assets/Scripts/HighscorePlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+[Serializable]
+public class HighscorePlayer
+{
+
+
+    public int Score { get; set; }
+
+    public string Nickname { get; set; }
+
+
+    public HighscorePlayer(int highscore, string nickname)
+    {
+        this.Score = highscore;
+        this.Nickname = nickname;
+    }
+
+
+
+
+
+}
