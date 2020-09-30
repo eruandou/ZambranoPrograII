96da8035795f4c725667bd9c063db271783dd3cb
eruandou <zambra_nacho@hotmail.com>
2020-09-30 19:55:45 -0300

    Test Json and scores showing class

---
+++ Assets/Scripts/PlayersFromHighScoreRow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayersFromHighScoreRow : MonoBehaviour
+{
+    public Text Nickname;    
+    public Text Score;
+
+    public void SetData(HighscorePlayer player)
+    {
+        Nickname.text = player.nickname;
+        Score.text = player.score.ToString();
+    }
+
+    public void ResetData()
+    {
+        Nickname.text = "---";
+        Score.text = "---";
+    }
+}
