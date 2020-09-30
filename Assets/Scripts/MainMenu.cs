994200b1e00e5c5a174f1e13efba77f8c1112173
eruandou <zambra_nacho@hotmail.com>
2020-09-30 19:53:22 -0300

    Main menu interactions scripts

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject mainMenuPanel;
+    [SerializeField] private GameObject creditsPanel;
+    [SerializeField] private GameObject highScorePanel;
+    [SerializeField] private GameObject lowScoresPanel;
+
+    [SerializeField] private PlayersFromHighScoreRow[] hallOfFame;
+    [SerializeField] private PlayersFromHighScoreRow[] hallOfInfamy;
+
+    [SerializeField] private GameObject warning;
+
+    private void Start()
+    {
+        ToMainMenu();
+
+        DataSaver.Load();
+
+        PopulateHighscoreLists();
+    }
+
+    private void PopulateHighscoreLists()
+    {
+        if (!DataSaver.BSTHighscores.IsTreeEmpty())
+        {
+            List<HighscorePlayer> BestScorePlayers = new List<HighscorePlayer>();
+            List<HighscorePlayer> WorstScorePlayers = new List<HighscorePlayer>();
+
+            int topOfList;
+            if (DataSaver.BSTHighscores.nodeCount >= 5) topOfList = 5;
+            else topOfList = DataSaver.BSTHighscores.nodeCount;
+
+            for (int i = 0; i < topOfList; i++)
+            {
+                hallOfFame[i].SetData(DataSaver.BSTHighscores.InReverseOrder(DataSaver.BSTHighscores.root, BestScorePlayers)[i]);
+                hallOfInfamy[i].SetData(DataSaver.BSTHighscores.InOrder(DataSaver.BSTHighscores.root, WorstScorePlayers)[i]);
+            }
+            
+
+        }
+
+        else
+        {
+            Debug.Log("Tree is empty");
+        }
+    }
+
+    public void ReUpdateHighscoreLists()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            hallOfFame[i].ResetData();
+            hallOfInfamy[i].ResetData();
+        }
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("TestLevel");
+        if (Gamemanager.instance != null) Gamemanager.instance.OnLevelLoad();
+    }
+
+    public void ToMainMenu()
+    {
+        mainMenuPanel.SetActive(true);
+        creditsPanel.SetActive(false);
+        highScorePanel.SetActive(false);
+        lowScoresPanel.SetActive(false);
+    }
+
+    public void ToCredits()
+    {
+        mainMenuPanel.SetActive(false);
+        creditsPanel.SetActive(true);
+        highScorePanel.SetActive(false);
+        lowScoresPanel.SetActive(false);
+    }
+
+    public void ToHighscores()
+    {
+        mainMenuPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+        highScorePanel.SetActive(true);
+    }
+
+    public void ToHallOfInfamy()
+    {
+        highScorePanel.SetActive(false);
+        lowScoresPanel.SetActive(true);
+    }
+
+    public void ToHallOfFame()
+    {
+        highScorePanel.SetActive(true);
+        lowScoresPanel.SetActive(false);
+    }
+    
+    public void Exit()
+    {
+        Application.Quit();
+        Debug.Log("Quitting app");
+    }
+
+    public void ResetScores()
+    {
+        highScorePanel.SetActive(false);
+        lowScoresPanel.SetActive(false);
+        warning.SetActive(true);
+        warning.GetComponent<Warning>().warning.SetActive(true);
+    }
+
+
+}
