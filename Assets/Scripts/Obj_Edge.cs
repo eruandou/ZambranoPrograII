64649b7f9f5b51f5a54b5e4c442df2d2252d2173
eruandou <zambra_nacho@hotmail.com>
2020-11-02 17:01:32 -0300

    Edge container class

---
+++ Assets/Scripts/Obj_Edge.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obj_Edge
+{
+
+    public int id;
+
+    public int idOrig;
+    public int idDest;
+    public int value;
+
+
+
+
+}
