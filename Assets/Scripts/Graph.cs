f01b2c1d66352aa869319bd7e50a3ea42d5b45e8
eruandou <zambra_nacho@hotmail.com>
2020-11-02 16:50:10 -0300

    Add graph and needed classes (set-dijkstra)

---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public interface IGraph
+{
+    void InitializeGraph();
+    void AddVertex(int vertex);
+    void RemoveVertex(int vertex);
+    ISet Vertexes();
+    void AddEdge(int id,int vertex1, int vertex2, int value);
+    void RemoveEdge(int vertex1, int vertex2);
+    bool DoesEdgeExist(int vertex1, int vertex2);
+    int EdgeValue(int vertex1, int vertex2);
+}
+
+public class Graph : IGraph
+{
+
+    static int matrixSize = 100;
+    public int[,] adjacencyMatrix;
+    public int[] labels;
+    public int nodeCount;
+    public int[] nodes1;
+    public int[] nodes2;
+
+    public Graph()
+    {
+        InitializeGraph();
+    }
+
+    public void InitializeGraph()
+    {   //Initialize values
+        adjacencyMatrix = new int[matrixSize,matrixSize];
+        labels = new int[matrixSize];
+        nodeCount = 0;
+    }
+
+    public void AddVertex(int vertex)
+    {
+        //Add label, and set its initial values to 0
+        labels[nodeCount] = vertex;
+        for (int i = 0; i <= nodeCount; i++)
+        {
+            adjacencyMatrix[nodeCount, i] = 0;
+            adjacencyMatrix[i, nodeCount] = 0;
+        }
+        
+        nodeCount++;
+    }
+
+    public void RemoveVertex(int vertexToRemove)
+    {
+        //Check which vertex we need to remove
+        int index = Vertex2Index(vertexToRemove);
+
+        //Reorganize the adjacency matrix based on removed vertex
+        for (int j = 0; j < nodeCount; j++)
+        {
+            adjacencyMatrix[j, index] = adjacencyMatrix[j, nodeCount - 1];
+        }
+
+        for (int j = 0; j < nodeCount; j++)
+        {
+            adjacencyMatrix[index, j] = adjacencyMatrix[nodeCount - 1, j];
+        }
+
+        //Reorganize label system
+        labels[index] = labels[nodeCount - 1];
+        nodeCount--;
+    }
+
+    public int Vertex2Index(int vertex)
+    {
+        int i = nodeCount - 1;
+        //based on vertex, check the labels for
+        //required vertex to return
+        while (i >= 0 && labels[i] != vertex)
+        {
+            i--;
+        }
+
+        return i;
+    }
+
+    public ISet Vertexes()
+    {
+        //create vertexes set for each existing node
+        ISet Vertexes = new Set();
+    
+        for (int i = 0; i < nodeCount; i++)
+        {
+            Vertexes.Add(labels[i]);
+        }
+        return Vertexes;
+    }
+
+    public void AddEdge(int id,int vertex1, int vertex2, int value)
+    {
+        //Given two vertexes, adds value to the edge 
+        //between them
+        int j = Vertex2Index(vertex1);
+        int k = Vertex2Index(vertex2);
+        adjacencyMatrix[j, k] = value;
+    }
+
+    public void RemoveEdge(int vertex1, int vertex2)
+    {
+        //Given two vertexes, removes the value of
+        //the edge between them
+        int j = Vertex2Index(vertex1);
+        int k = Vertex2Index(vertex2);
+        adjacencyMatrix[j, k] = 0;
+    }
+
+    public bool DoesEdgeExist(int vertex1, int vertex2)
+    {
+        //checks if the edge value between two vertexes
+        //is not 0.
+        int j = Vertex2Index(vertex1);
+        int k = Vertex2Index(vertex2);
+        return adjacencyMatrix[j, k] != 0;
+    }
+
+    public int EdgeValue(int vertex1, int vertex2)
+    {
+        //Returns edge value between two given
+        //vertexes+
+        int j = Vertex2Index(vertex1);
+        int k = Vertex2Index(vertex2);
+        return adjacencyMatrix[j, k];
+    }
+
+    public void Dijkstra(int sourceNode)
+    {
+      (nodes1, nodes2) = AlgDijkstra.Dijkstra(this, sourceNode);
+    }
+}
---
+++ Assets/Scripts/Set.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ISet
+{
+    void InitializeSet();
+    bool IsSetEmpty();
+    void Add(int x);
+    int Pick();
+    void Remove(int x);
+    bool Belongs(int x);
+}
+
+public class Set: ISet
+{
+
+    int[] vertex;
+    int position;
+
+
+    public Set()
+    {
+        InitializeSet();
+    }
+
+
+
+
+    public void InitializeSet()
+    {
+        vertex = new int[100];
+        position = 0;
+    }
+    public bool IsSetEmpty()
+    {
+        return position == 0;
+    }
+
+    public void Add(int newVertex)
+    {
+        if (!this.Belongs(newVertex))
+        {
+            vertex[position] = newVertex;
+            position++;
+        }
+    }
+
+
+    public int Pick()
+    {
+        return vertex[position - 1];
+    }
+
+   
+
+    public void Remove(int vertexToRemove)
+    {
+        int i = 0;
+        while (i < position && vertex[i] != vertexToRemove)
+        {
+            i++;
+        }
+        if (i < position)
+        {
+            vertex[i] = vertex[position - 1];
+            position--;
+        }
+    }
+    public bool Belongs(int x)
+    {
+        int i = 0;
+        while (i < position && vertex[i] != x)
+        {
+            i++;
+        }
+        return (i < position);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
