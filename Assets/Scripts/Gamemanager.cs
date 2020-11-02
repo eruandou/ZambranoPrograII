e4b1116ed9e1c058e9d92e0367c363e9f419ccb4
eruandou <zambra_nacho@hotmail.com>
2020-11-02 20:05:12 -0300

    Checks csv for level data + changes to load scene

--- Assets/Scripts/Gamemanager.cs
+++ Assets/Scripts/Gamemanager.cs
@@ -13,7 +13,11 @@ public class Gamemanager : MonoBehaviour
 
     public int lastAccessedLevel = 1;
 
-    
+    private TextAsset levelData;
+
+    public int MaxEnemies { get; private set; }
+
+    public float TimeLimit { get; private set; }
 
     public int ActualPoints { get; private set; }
 
@@ -30,6 +34,8 @@ public class Gamemanager : MonoBehaviour
         }
 
         DontDestroyOnLoad(this);
+
+        levelData = Resources.Load<TextAsset>("LevelData");        
     }
 
     public void OnGameStart()
@@ -61,7 +67,28 @@ public class Gamemanager : MonoBehaviour
         UI.UpdatePoints();
     }
 
+    public void LoadScene(string levelToLoad)
+    {
+        SceneManager.LoadSceneAsync(levelToLoad);
+    }
 
+    public void LoadLevel (int levelToLoad)
+    {
+        string[] data = levelData.text.Split(new char[] {'\n' });
+
+       string [] row = data[levelToLoad].Split(new char[] { ',' });
+
+        int.TryParse(row[1], out int maxEnemies);
+        MaxEnemies = maxEnemies;
+
+        float.TryParse(row[2], out float timeLimit);
+        TimeLimit = timeLimit;
+
+        lastAccessedLevel = levelToLoad;
+
+        LoadScene($"Level{levelToLoad}");
+    }
+    
     public void LoadGameOver()
     {
         SceneManager.LoadScene("GameOverScene");
