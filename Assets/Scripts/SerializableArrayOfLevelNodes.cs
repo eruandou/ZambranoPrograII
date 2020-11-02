db2c9272370cb8e8ea22ae9e0f1082bdee795337
eruandou <zambra_nacho@hotmail.com>
2020-11-02 17:03:52 -0300

    Serializable class of level nodes

---
+++ Assets/Scripts/SerializableArrayOfLevelNodes.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class SerializableArrayOfLevelNodes 
+{
+    public bool[] unlockStates;
+
+    public SerializableArrayOfLevelNodes()
+    {
+        unlockStates = new bool[11];
+        for (int i = 1; i < unlockStates.Length; i++)
+        {
+            unlockStates[i] = false;
+        }
+
+        unlockStates[0] = true;
+    }
+    
+       
+
+}
