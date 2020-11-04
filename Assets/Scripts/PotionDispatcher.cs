80efe4613b735847c41eb28814e64a09a5c446e4
eruandou <zambra_nacho@hotmail.com>
2020-11-04 17:42:32 -0300

    Added potion factory

---
+++ Assets/Scripts/PotionDispatcher.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PotionDispatcher: MonoBehaviour
+{
+
+    private GameObject moveP, healP, freezeP;
+
+    private static PotionDispatcher instance;
+      
+
+    //Create singleton
+    public static PotionDispatcher Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new PotionDispatcher();
+            }
+            return instance;
+        }
+
+       
+    }
+
+    
+
+    public enum PotionRequired
+    {
+        move,
+        heal,
+        freeze
+    }
+
+    public PotionDispatcher()
+    {
+        freezeP = Resources.Load("Prefabs/Items/Freeze Potion") as GameObject;
+        healP = Resources.Load("Prefabs/Items/Heal Potion") as GameObject;    
+        moveP = Resources.Load("Prefabs/Items/Move Potion") as GameObject;
+        
+    }
+
+    public GameObject GetPotion(PotionRequired potionType)
+    {
+        switch (potionType)
+        {
+            case PotionRequired.move:
+                return moveP; 
+
+            case PotionRequired.heal:
+                return healP;
+
+            case PotionRequired.freeze:
+                return freezeP;
+
+            default:
+                return moveP;
+        }
+    }
+
+
+
+
+
+}
