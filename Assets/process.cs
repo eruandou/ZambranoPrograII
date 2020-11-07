298d15bd9bf3e75d45928f54c82f87a0a8ef3fca
eruandou <zambra_nacho@hotmail.com>
2020-11-07 15:09:58 -0300

    Small touches to lvl select and level1

---
+++ Assets/process.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class process : MonoBehaviour
+{
+
+    public Shader shader;
+
+    void Start()
+    {       
+        //Camera.main.RenderWithShader(shader, "GB");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Camera.main.RenderWithShader(shader, "GB");
+    }
+    
+  
+}
