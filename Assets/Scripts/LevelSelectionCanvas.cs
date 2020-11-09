7617e6c57d44933967abc981f10f80002b901d9c
eruandou <zambra_nacho@hotmail.com>
2020-11-08 21:33:57 -0300

    Level select option to wipe data

---
+++ Assets/Scripts/LevelSelectionCanvas.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelectionCanvas : MonoBehaviour
+{
+
+    [SerializeField] private GameObject clearPanel, confirmPanel;
+
+
+
+
+    private void Start()
+    {
+        ToMain();
+    }
+
+
+
+
+
+
+
+
+    public void ToConfirm()
+    {
+        clearPanel.SetActive(false);
+        confirmPanel.SetActive(true);
+    }
+
+    public void ToMain()
+    {
+        clearPanel.SetActive(true);
+        confirmPanel.SetActive(false);
+    }
+
+
+    public void ClearData()
+    {
+        LevelsSaver.ClearData();
+        ToMain();
+        Gamemanager.instance.LoadScene("LevelSelect");
+    }
+
+
+
+}
