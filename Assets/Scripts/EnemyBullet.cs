5b574f587319bd72d7f4d2a6fd04c31e4328d722
eruandou <zambra_nacho@hotmail.com>
2020-12-13 20:40:36 -0300

    Flying Enemy bullet

---
+++ Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    private Vector2 direction;
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
