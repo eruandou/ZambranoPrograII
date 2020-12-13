44a7ee82bc0467549139a7f1c3a40e7fef85a55d
eruandou <zambra_nacho@hotmail.com>
2020-12-13 15:36:41 -0300

    Radar script

---
+++ Assets/Scripts/Radar.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radar : MonoBehaviour
+{
+    private EnemiesController enemyCont;
+
+    private Enemy closestEnemy;
+
+    private Vector2 pointDirection;
+
+    [SerializeField] private GameObject radarUi;
+    [SerializeField] private RectTransform radarPointer;
+
+
+
+
+    private void Start()
+    {
+        enemyCont = FindObjectOfType<EnemiesController>();
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            radarUi.SetActive(true);
+            closestEnemy = enemyCont.ReturnClosestEnemy();           
+        }
+
+        if (Input.GetKeyUp (KeyCode.Tab))
+        {
+            radarUi.SetActive(false);
+        }
+
+        if (closestEnemy != null)
+        {
+            float angle = Vector2.Angle(transform.position, closestEnemy.transform.position);
+
+            radarPointer.localEulerAngles = new Vector3(0, 0, angle);
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
