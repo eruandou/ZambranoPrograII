4381f4c1aedfe5f9b9834f4913cc02e5ef04dd69
eruandou <zambra_nacho@hotmail.com>
2020-12-16 20:28:14 -0300

    Added Secret item with spawning Tile

---
+++ Assets/CollectableSecretItem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableSecretItem : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+
+        var player = collision.GetComponent<Player>();
+
+        if (player != null)
+        {
+            Gamemanager.instance.GetSecretItem();
+            Destroy(gameObject); 
+        }
+    }
+}
---
+++ Assets/destructableTile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destructableTile : MonoBehaviour
+{
+
+    [SerializeField] private GameObject colletableSecretItem;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Bullet bullet = collision.GetComponent<Bullet>();
+
+        if (bullet != null)
+        {
+            Instantiate(colletableSecretItem, this.transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+
+
+
+
+
+
+}
