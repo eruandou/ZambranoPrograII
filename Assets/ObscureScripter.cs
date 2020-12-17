be781595b1d8ae04e098551ceaa962cec907ae3a
eruandou <zambra_nacho@hotmail.com>
2020-12-17 03:33:12 -0300

    Dark ambience post processing volume added

---
+++ Assets/ObscureScripter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+
+public class ObscureScripter : MonoBehaviour
+{
+    private PostProcessVolume ppVol;
+    private Vignette vignette;
+
+
+    [Range(0, 1)] [SerializeField] private float intensity;
+    [Range(0, 1)] [SerializeField] private float smoothness;
+
+    private void Start()
+    {
+        ppVol = GetComponent<PostProcessVolume>();
+        vignette = ppVol.profile.settings[0] as Vignette;
+        vignette.intensity.value = intensity;
+        vignette.smoothness.value = this.smoothness;
+    }
+
+
+
+
+
+
+}
