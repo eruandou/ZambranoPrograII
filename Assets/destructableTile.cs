607f10071a09e68b039b6b388847922c93e2c4fc
eruandou <zambra_nacho@hotmail.com>
2020-12-17 03:32:38 -0300

    Destructable tile added

--- Assets/destructableTile.cs
+++ Assets/destructableTile.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,9 @@ public class destructableTile : MonoBehaviour
 {
 
     [SerializeField] private GameObject colletableSecretItem;
+    [SerializeField] private int collectableSecretItemNumber;
+
+    public event Action OnDestroyTile;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -13,7 +17,9 @@ public class destructableTile : MonoBehaviour
 
         if (bullet != null)
         {
-            Instantiate(colletableSecretItem, this.transform.position, Quaternion.identity);
+            OnDestroyTile?.Invoke();
+            var c = Instantiate(colletableSecretItem, this.transform.position, Quaternion.identity);
+            c.GetComponent<CollectableSecretItem>().GetItemNumber(collectableSecretItemNumber);
             Destroy(gameObject);
         }
     }
