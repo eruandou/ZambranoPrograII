6ab387d9313549f77f3a4ee917b11401c08b2fef
eruandou <zambra_nacho@hotmail.com>
2020-12-17 03:35:24 -0300

    Last level only item neede for story

---
+++ Assets/FinalBoxSurpirse.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalBoxSurpirse : MonoBehaviour
+{
+
+    private EnemiesController enemiesCont;
+    private Enemy fakeEnemyThis;
+    private destructableTile dTile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemiesCont = FindObjectOfType<EnemiesController>();
+        fakeEnemyThis = GetComponent<Enemy>();
+        enemiesCont.OnEnemySpawnHandler(fakeEnemyThis);
+        dTile = GetComponent<destructableTile>();
+        dTile.OnDestroyTile += () =>
+        {
+            enemiesCont.OnEnemyDieHandler(fakeEnemyThis);         
+        };        
+    }
+
+   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+
+}
